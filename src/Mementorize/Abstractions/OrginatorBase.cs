a0a8e30b63035306344438a614e11a6903037821
bardia <mstbardia@gmail.com>
2021-08-06 17:39:59 +0430

    orginator abstraction added

---
+++ src/Mementorize/Abstractions/OrginatorBase.cs
@@ -0,0 +1,27 @@
+﻿using Mementorize.Models;
+
+namespace Mementorize.Abstractions
+{
+    public abstract class OrginatorBase<T> where T : new()
+    {
+        protected T State { get; set; }
+
+        public virtual void SetState(T state)
+        {
+            State = state;
+        }
+
+        public virtual T GetState()
+        {
+            return State;
+        }
+        
+        public virtual Memento<T> CreateMemento()
+        {
+            var memento = new Memento<T>();
+            memento.SetState(State);
+            return memento;   
+        }
+        
+    }
+}
--- src/Mementorize/Models/Orginator.cs
+++ src/Mementorize/Models/Orginator.cs
@@ -1,24 +1,9 @@
-﻿namespace Mementorize.Models
+﻿using Mementorize.Abstractions;
+
+namespace Mementorize.Models
 {
-    public abstract class Orginator<T> where T : new()
+    public abstract class Orginator<T> : OrginatorBase<T> where T : new()
     {
-        protected T State { get; set; }
         
-        public virtual void SetState(T state)
-        {
-            State = state;
-        }
-        
-        public virtual T GetState()
-        {
-            return State;
-        }
-        
-        public virtual Memento<T> CreateMemento()
-        {
-            var memento = new Memento<T>();
-            memento.SetState(State);
-            return memento;
-        }
     }
 }
